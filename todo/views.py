181703e82c1a97ddcf8fac05a3d7cef741d5340f
BAAZIZ KHADIDJA <baazize-khadija@hotmail.com>
2021-03-22 09:02:03 +0100

    first commit

---
+++ todo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from todo.models import Todo
+
+def general(request):
+    taches = Todo.objects.all()
+    return render(request,'todo.html', {'taches': taches})
+
+def addTodo(request):
+    new_elt = Todo(name = request.POST['content'])
+    new_elt.save()
+    return HttpResponseRedirect('/general/')
+
+
+def deleteTodo(request, todo_id):
+    elt_todelete = Todo.objects.get(id = todo_id)
+    elt_todelete.delete()
+    return HttpResponseRedirect('/general/')
